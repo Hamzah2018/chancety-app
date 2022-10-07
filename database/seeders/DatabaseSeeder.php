ea3064654e8a75341721bb7c4dd71d466957cec6
Hamzah Al-Hagi <ha.773774141@gmail.com>
2022-10-07 13:14:23 +0300

    Fix dashbaord links

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-        AdminSeeder::class,
-        CustomerSeeder::class,
+            AdminSeeder::class,
+            CustomerSeeder::class,
     ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,17 +18,23 @@ class UserSeeder extends Seeder
               //
               User::create([
                 'name' =>'User',
+                'fname' =>'المستخدم ',
+                'lname' =>'الاول',
                 'email' => 'user@gmail.com',
                 'password' => Hash::make('123456789'),
                 ]);
 
                 User::create([
                 'name' =>'User2',
+                'fname' =>'المستخدم ',
+                'lname' =>'الثاني',
                 'email' => 'user2@gmail.com',
                 'password' => Hash::make('123456789'),
                 ]);
                 User::create([
                 'name' =>'us',
+                'fname' =>'المستخدم ',
+                'lname' =>'الثالث',
                 'email' => 'us@gmail.com',
                 'password' => Hash::make('123456789'),
                 ]);
